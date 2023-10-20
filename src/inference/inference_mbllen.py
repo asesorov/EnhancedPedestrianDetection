bbc0fc09d55b03a55f732139ab4d05f42d066f78
asesorov <crafter2022@gmail.com>
2023-10-20 16:49:27 +0300

    Fix LFS

--- src/inference/inference_mbllen.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bd93471ec44a7ce8ade2fc1914afc211e926c20f1acbad4764d987cf5d8f8640
-size 1337
--- src/inference/models/mirnet.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:374ad6d2940606cfc37fe72b1f446dc0cecf19c7288deb5730b8226cbfa2558e
-size 15398
