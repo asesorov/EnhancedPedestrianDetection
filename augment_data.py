0226a1326fda09ac7fb03f12ffbedfff45dfe002
Арсений Казанцев <kazanzev123@yandex.ru>
2023-10-25 17:18:48 +0300

    Dataset version 0.0.3

--- augment_data.py
+++ augment_data.py
@@ -5,6 +5,7 @@ import argparse
 import albumentations as A
 import cv2
 
+
 def apply_augmentations(input_dir, augmentations):
     files = [f for f in listdir(input_dir) if isfile(join(input_dir, f))]
         
@@ -14,15 +15,17 @@ def apply_augmentations(input_dir, augmentations):
         augmented_image = A.Compose(augmentations)(image=image)['image']
         cv2.imwrite(f, augmented_image)
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input-dir', type=str, required=True)
     args = parser.parse_args()
 
     transform = A.Compose([
-        A.CLAHE(always_apply=True, p=1.0, clip_limit=(1, 2), tile_grid_size=(2, 2)),
-        A.RandomBrightness(always_apply=True, p=1.0, limit=(0.15, 0.15)),
-        A.RandomContrast(always_apply=True, p=1.0, limit=(0.2, 0.2))
+        A.CLAHE(always_apply=True, p=1.0, clip_limit=1.6260416991257398, tile_grid_size=(2, 2)),
+        A.Equalize(always_apply=True, p=1.0),
+        A.ColorJitter(always_apply=True, p=1.0, brightness=0.18760935947326096,
+                      contrast=0.2968125662449388, saturation=0, hue=0),
     ])
 
     apply_augmentations(args.input_dir, transform)
