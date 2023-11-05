1213596bbc55423b377d3ebb08d07af806f41207
Egorbl <egor.belyakovvvv@gmail.com>
2023-11-05 19:40:45 +0300

    Added logic on models combination to streamlit

--- app.py
+++ app.py
@@ -24,41 +24,62 @@ st.title("Object Detection using YOLOv8")
 st.sidebar.header("ML Model Config")
 
 # Model Options
-model_type = st.sidebar.radio(
+model_types = st.sidebar.multiselect(
     "Select Task", ['Detection', 'Segmentation'])
+model_detection_path = None
+model_segmentation_path = None
+model_detection = None
+model_segmentation = None
+include_logic = False
+
+if "Detection" in model_types and "Segmentation" in model_types:
+    include_logic = st.sidebar.checkbox(label="Include logic")
 
 confidence = float(st.sidebar.slider(
-    "Select Model Confidence", 10, 100, 40)) / 100
+    "Select Model Confidence", 10, 100, 35)) / 100
 
 st.sidebar.header("Video Config")
 source_radio = st.sidebar.radio(
     "Select Source", settings.SOURCES_LIST)
 
 # Selecting Detection Or Segmentation
-if model_type == 'Detection':
-    model_path = Path(settings.DETECTION_MODEL)
-elif model_type == 'Segmentation':
-    model_path = Path(settings.SEGMENTATION_MODEL)
+if 'Detection' in model_types:
+    model_detection_path = Path(settings.DETECTION_MODEL)
+if 'Segmentation' in model_types:
+    model_segmentation_path = Path(settings.SEGMENTATION_MODEL)
 
 # Load Pre-trained ML Model
-try:
-    model = helper.load_model(model_path)
-except Exception as ex:
-    st.error(f"Unable to load model. Check the specified path: {model_path}")
-    st.error(ex)
+if model_detection_path:
+    try:
+        model_detection = helper.load_model(model_detection_path)
+    except Exception as ex:
+        st.error(f"Unable to load model. Check the specified path: {model_detection_path}")
+        st.error(ex)
 
+if model_segmentation_path:
+    try:
+        model_segmentation = helper.load_model(model_segmentation_path)
+    except Exception as ex:
+        st.error(f"Unable to load model. Check the specified path: {model_segmentation_path}")
+        st.error(ex)
+
+helper_func = None
 
 if source_radio == settings.VIDEO:
-    helper.play_stored_video(confidence, model)
+    helper_func = helper.play_stored_video
 
 elif source_radio == settings.WEBCAM:
-    helper.play_webcam(confidence, model)
+    helper_func = helper.play_webcam
 
 elif source_radio == settings.RTSP:
-    helper.play_rtsp_stream(confidence, model)
+    helper_func = helper.play_rtsp_stream
 
 elif source_radio == settings.YOUTUBE:
-    helper.play_youtube_video(confidence, model)
+    helper_func = helper.play_youtube_video
 
 else:
     st.error("Please select a valid source type!")
+
+if helper_func:
+    helper_func(confidence, model_detection=model_detection,
+                model_segmentation=model_segmentation, include_logic=include_logic)
--- helper.py
+++ helper.py
@@ -1,10 +1,11 @@
 from ultralytics import YOLO
-import time
 import streamlit as st
 import cv2
 from pytube import YouTube
 
 import settings
+import numpy as np
+from ultralytics.utils.ops import scale_image
 import math
 
 
@@ -13,7 +14,7 @@ def load_model(model_path):
     Loads a YOLO object detection model from the specified model_path.
 
     Parameters:
-        model_path (str): The path to the YOLO model file.
+        model_path: The path to the YOLO model file.
 
     Returns:
         A YOLO object detection model.
@@ -24,14 +25,15 @@ def load_model(model_path):
 
 def display_tracker_options():
     display_tracker = st.checkbox(label="Tracker??")
-    is_display_tracker = True if display_tracker == True else False
+    is_display_tracker = display_tracker
     if is_display_tracker:
         tracker_type = st.radio("Tracker", ("bytetrack.yaml", "botsort.yaml"))
         return is_display_tracker, tracker_type
     return is_display_tracker, None
 
 
-def _display_detected_frames(conf, model, st_frame, image, is_display_tracking=None, tracker=None):
+def _display_detected_frames(conf, st_frame, image, is_display_tracking=None, tracker=None,
+                             model_detection=None, model_segmentation=None, include_logic=False):
     """
     Display the detected objects on a video frame using the YOLOv8 model.
 
@@ -45,19 +47,53 @@ def _display_detected_frames(conf, model, st_frame, image, is_display_tracking=N
     Returns:
     None
     """
-
     # Resize the image to a standard size
-    image = cv2.resize(image, (720, int(720*(9/16))))
+    image = cv2.resize(image, (720, int(720 * (9 / 16))))
+    res_detection = None
+    res_segmentation = None
 
     # Display object tracking, if specified
-    if is_display_tracking:
-        res = model.track(image, conf=conf, persist=True, tracker=tracker)
+    if model_detection:
+        if is_display_tracking:
+            res_detection = model_detection.track(image, conf=conf, persist=True, tracker=tracker)
+        else:
+            # Predict the objects in the image using the YOLOv8 model
+            res_detection = model_detection.predict(image, conf=conf)
+
+    if model_segmentation:
+        if is_display_tracking:
+            res_segmentation = model_segmentation.track(image, conf=conf, persist=True, tracker=tracker)
+        else:
+            # Predict the objects in the image using the YOLOv8 model
+            res_segmentation = model_segmentation.predict(image, conf=conf)
+
+    if res_detection:
+        boxes = res_detection[0].boxes.xyxy.numpy().astype("int16")
     else:
-        # Predict the objects in the image using the YOLOv8 model
-        res = model.predict(image, conf=conf)
+        boxes = []
+
+    if res_segmentation:
+        segmentation_mask = get_resized_mask(res_segmentation)
+        segmentation_mask = merge_masks(segmentation_mask)
+        if not res_segmentation[0].masks:
+            segment_areas = []
+        else:
+            segment_areas = res_segmentation[0].masks.xy
+            segment_areas = merge_polygons(segment_areas)
+    else:
+        segmentation_mask = np.zeros([*image.shape[:2], 1])
+        segment_areas = []
+
+    if include_logic:
+        new_boxes = []
+        for box in boxes:
+            if not if_person_on_road(box, segmentation_mask, segment_areas):
+                continue
+            new_boxes.append(box)
+        boxes = new_boxes
 
     # # Plot the detected objects on the video frame
-    res_plotted = res[0].plot()
+    res_plotted = plot_detect_segment(image, boxes, segmentation_mask)
     st_frame.image(res_plotted,
                    caption='Detected Video',
                    channels="BGR",
@@ -66,7 +102,7 @@ def _display_detected_frames(conf, model, st_frame, image, is_display_tracking=N
     return res_plotted
 
 
-def play_youtube_video(conf, model):
+def play_youtube_video(*args, **kwargs):
     """
     Plays a webcam stream. Detects Objects in real-time using the YOLOv8 object detection model.
 
@@ -91,15 +127,15 @@ def play_youtube_video(conf, model):
             vid_cap = cv2.VideoCapture(stream.url)
 
             st_frame = st.empty()
-            while (vid_cap.isOpened()):
+            while vid_cap.isOpened():
                 success, image = vid_cap.read()
                 if success:
-                    _display_detected_frames(conf,
-                                             model,
+                    _display_detected_frames(*args,
                                              st_frame,
                                              image,
                                              is_display_tracker,
                                              tracker,
+                                             **kwargs
                                              )
                 else:
                     vid_cap.release()
@@ -108,7 +144,7 @@ def play_youtube_video(conf, model):
             st.sidebar.error("Error loading video: " + str(e))
 
 
-def play_rtsp_stream(conf, model):
+def play_rtsp_stream(*args, **kwargs):
     """
     Plays an rtsp stream. Detects Objects in real-time using the YOLOv8 object detection model.
 
@@ -129,15 +165,15 @@ def play_rtsp_stream(conf, model):
         try:
             vid_cap = cv2.VideoCapture(source_rtsp)
             st_frame = st.empty()
-            while (vid_cap.isOpened()):
+            while vid_cap.isOpened():
                 success, image = vid_cap.read()
                 if success:
-                    _display_detected_frames(conf,
-                                             model,
+                    _display_detected_frames(*args,
                                              st_frame,
                                              image,
                                              is_display_tracker,
-                                             tracker
+                                             tracker,
+                                             **kwargs
                                              )
                 else:
                     vid_cap.release()
@@ -150,7 +186,7 @@ def play_rtsp_stream(conf, model):
             st.sidebar.error("Error loading RTSP stream: " + str(e))
 
 
-def play_webcam(conf, model):
+def play_webcam(*args, **kwargs):
     """
     Plays a webcam stream. Detects Objects in real-time using the YOLOv8 object detection model.
 
@@ -170,15 +206,15 @@ def play_webcam(conf, model):
         try:
             vid_cap = cv2.VideoCapture(source_webcam)
             st_frame = st.empty()
-            while (vid_cap.isOpened()):
+            while vid_cap.isOpened():
                 success, image = vid_cap.read()
                 if success:
-                    _display_detected_frames(conf,
-                                             model,
+                    _display_detected_frames(*args,
                                              st_frame,
                                              image,
                                              is_display_tracker,
                                              tracker,
+                                             **kwargs
                                              )
                 else:
                     vid_cap.release()
@@ -187,7 +223,7 @@ def play_webcam(conf, model):
             st.sidebar.error("Error loading video: " + str(e))
 
 
-def play_stored_video(conf, model):
+def play_stored_video(*args, **kwargs):
     """
     Plays a stored video file. Tracks and detects objects in real-time using the YOLOv8 object detection model.
 
@@ -217,17 +253,17 @@ def play_stored_video(conf, model):
                 str(settings.VIDEOS_DICT.get(source_vid)))
             st_frame = st.empty()
             count = 0
-            while (vid_cap.isOpened()):
+            while vid_cap.isOpened():
                 count += 1
                 success, image = vid_cap.read()
                 if success:
-                    res = _display_detected_frames(conf,
-                                             model,
-                                             st_frame,
-                                             image,
-                                             is_display_tracker,
-                                             tracker
-                                             )
+                    res = _display_detected_frames(*args,
+                                                   st_frame,
+                                                   image,
+                                                   is_display_tracker,
+                                                   tracker,
+                                                   **kwargs
+                                                   )
                     count_str = "0" * (4 - len(str(count))) + str(count)
                     cv2.imwrite(f'processed_images/color_img_{count_str}.png', res)
                 else:
@@ -237,33 +273,63 @@ def play_stored_video(conf, model):
             st.sidebar.error("Error loading video: " + str(e))
 
 
-def dist_alert(bbox_pesh_pred, seg_road_pred,threshold):
-    #тут вопрос -- как будут отображаться в ббоксе несколько пешеходов (не успею найти нужный пример), сейчас для одного (на mvp)
-    for crd in bbox_pesh_pred:
-        bbox_coord = crd.boxes.xyxy[0]
+def get_resized_mask(yolo_segment_res):
+    yolo_segment_res = yolo_segment_res[0]
 
-    pesh_x = (bbox_coord[2] + bbox_coord[0]) / 2 #середина нижнего по х
-    pesh_y = bbox_coord[3] #высота нижнего по y
+    if yolo_segment_res.masks is None:
+        return np.zeros([*yolo_segment_res.orig_shape, 1])
 
-    #если надо нормировать координаты (не совсем согласен с такой идеей, потому что не отталкиваемся от высоты и ширины - она же может варьроваться от исходника) то поделить соотв координаты
-    bboxes, classes, segmentations, scores = seg_road_pred
-    for bbox, class_id, road_seg, score in zip(bboxes, classes, segmentations, scores):
-        #достаем массив координат сегмента
-        road_seg = road_seg
+    result_mask = yolo_segment_res.masks.data.numpy()
+    result_mask = np.moveaxis(result_mask, 0, -1)
+    result_mask = scale_image(result_mask, yolo_segment_res.orig_shape)
 
-    #массив расстояний до сегмента
-    dist_list = []
-    for x,y in [road_seg]:
-        dist_list.append(math.sqrt((pesh_x - x) ** 2 + (pesh_y - y) ** 2))
+    return result_mask
 
-    # минимальное расстояние до сегмента
-    min_dist = min(dist_list)
 
-    #проверка : если находится на дороге
-    if ( pesh_x >= min(road_seg[0]) & pesh_x <= max(road_seg[0]) ) & ( (pesh_y >= min(road_seg[1]) & pesh_y <= max(road_seg[1])) ):
-        return ('alert', min_dist)
-    #проверка : если расстояние до ближайшего сегмента меньше порога
-    elif min_dist < threshold:
-        return ('alert', min_dist)
-    else:
-        return min_dist
+def draw_mask(image, mask_generated):
+    masked_image = image.copy()
+
+    masked_image = np.where(mask_generated.astype(int),
+                            np.array([255, 0, 0], dtype='uint8'),
+                            masked_image)
+
+    masked_image = masked_image.astype(np.uint8)
+
+    return cv2.addWeighted(image, 0.7, masked_image, 0.3, 0)
+
+
+def plot_detect_segment(image, bboxs_xyxy=[], segment_mask=None):
+    res_image = image.copy()
+
+    for bbox in bboxs_xyxy:
+        cv2.rectangle(res_image, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 255, 0), 1)
+
+    if segment_mask is None:
+        return res_image
+
+    return draw_mask(res_image, segment_mask)
+
+
+def if_person_on_road(bbox_xyxy, segment_mask, segment_polygons, threshold=2):
+    x_mean = (bbox_xyxy[2] + bbox_xyxy[0]) // 2
+    y_bot = bbox_xyxy[3]
+
+    if segment_mask[y_bot][x_mean] == 1:
+        return True
+
+    min_dist = float("+Inf")
+
+    for x, y in segment_polygons:
+        dist = math.sqrt((x_mean - x) ** 2 + (y_bot - y) ** 2)
+        min_dist = min(min_dist, dist)
+
+    return min_dist < threshold
+
+
+def merge_masks(mask):
+    result_mask = np.any(mask, axis=2)
+    return np.expand_dims(result_mask, axis=2)
+
+
+def merge_polygons(segment_polygons):
+    return np.concatenate(segment_polygons)
